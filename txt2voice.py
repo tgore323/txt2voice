6e77113d80f924dc7c878e58e775213b687a00ee
Tim Gore <tgore323@gmail.com>
2019-02-16 15:01:54 -0800

    Update txt2voice.py

--- txt2voice.py
+++ txt2voice.py
@@ -2,7 +2,8 @@
 # Program that takes text and turns it into speech. Speech is 
 # saved as an MP3 file for later processing. 
 # 
-# We'll be using Google's TTS
+# We'll be using Google's TTS. (pip3 install gtts)
+#
 from gtts import gTTS
 
 word = input("Enter text to be turned into speech: ")
@@ -10,8 +11,8 @@ tts = gTTS(text = word, lang = 'en')
 filename = word + ".mp3"
 tts.save(filename)
 
-
-if len(word) is >= 1:
-    print("long")
-else:
-    print("short")
+# Experimental workspace:
+# if len(word) is >= 1:
+#     print("long")
+# else:
+#     print("short")
