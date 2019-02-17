91fb6124b5dce99802cd38db21671c2665767b74
Tim Gore <tgore323@gmail.com>
2019-02-16 20:08:35 -0800

    Add files via upload

--- txt2voice.py
+++ txt2voice.py
@@ -11,8 +11,25 @@ tts = gTTS(text = word, lang = 'en')
 filename = word + ".mp3"
 tts.save(filename)
 
+
+
+
+
+
 # Experimental workspace:
-# if len(word) is >= 1:
-#     print("long")
-# else:
-#     print("short")
+
+word_list = []
+quantity = int(input("How many strings of text? "))
+quantity = quantity - 1
+
+
+
+# first_string = str(input("Enter string #1: "))
+# second_string = str(input("Enter string #2: "))
+# third_string = str(input("Enter string #3: "))
+
+
+for i in range(0, quantity):
+    word = str(input("Please enter a string: "))
+    word_list.append(word)
+
