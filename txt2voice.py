28443b5fd22b1159eac90325cb251907ad7a9ea0
Tim Gore <tgore323@gmail.com>
2019-02-22 15:06:11 -0800

    New version uploaded
    Added option to use text file for input. The option to manually enter text is still supported.

--- txt2voice.py
+++ txt2voice.py
@@ -1,33 +1,46 @@
 '''
 txt2voice by Tim Gore, KE6QBV
 
-txt2voice is a small python program to create voice files from inputed text. These
-voice files can then be converted to channel announcements for use Motorola two-way
-radios. 
+txt2voice is a small python program to create channel announcement
+files for use Motorola two-way radios. 
+
+INSTRUCTIONS:
 
 The text-to-speech engine used is Google's "gTTS". To install: 'pip3 install gtts'
+If choosing to import a text file, make sure you use the full filename. For example. if
+you want to import a text file called 'input' you would enter input.txt. The file must 
+be located in the same directory as this python file.
 
 This program has only been tested with linux. I have no desire to make it work in 
-Windows or MacOS
+Windows or MacOS. If you do, be my guest.
 
 To-do items:
-    - allow users to input a CSV or text file instead of entering each text string individually
     - Once files are made, do the nessassary conversion from inside program
+    - GUI?
+
 '''
 
 from gtts import gTTS
 
-# create a list and find out how many files to make
-list = []
-number = int(input("How many strings of text? "))
+print("Please choose how you would like to input text: \n")
+answer_1 = input("a) From a text file\nb) Manually\n:")
+if answer_1.lower() == "a":
+    list = []
+    txt_filename = str(input("Please enter file name: "))
+    list = open(txt_filename).read().splitlines()
+    for x in range(len(list)):
+        tts = gTTS(list[x], lang = "en")
+        filename = list[x] + ".mp3"
+        tts.save(filename)
+else:
+    list = []
+    number = int(input("How many strings of text? "))
+    for i in range(0, number):
+        word = str(input("Please enter a string: "))
+        list.append(word)
+    for x in range(len(list)):
+        tts = gTTS(list[x], lang = "en")
+        filename = list[x] + ".mp3"
+        tts.save(filename)
 
-# get text for desired voice file(s)
-for i in range(0, number):
-    word = str(input("Please enter a string: "))
-    list.append(word)
 
-# create voice files
-for x in range(len(list)):
-    tts = gTTS(list[x], lang = "en")
-    filename = list[x] + ".mp3"
-    tts.save(filename)
