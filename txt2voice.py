4164e25faad03b43c6a6fdc20b7e79f820b281d0
Tim Gore <tgore323@gmail.com>
2019-02-26 09:22:17 -0800

    new version 0.5.3

--- txt2voice.py
+++ txt2voice.py
@@ -1,21 +1,22 @@
 '''
 txt2voice by Tim Gore, KE6QBV@galvonix.com
-version 0.5.2    2/25/2018
+version 0.5.3    2/26/2018
 
 txt2voice is a small python program to create channel announcement
-files for use with Motorola two-way radios. Program creates MP3 files
+files for use with Motorola two-way radios. This program creates MP3 files
 which will need conversion to WAV files with proper formatting. (See
-to-do items).
+to-do items). 
 
 INSTRUCTIONS:
 
 The text-to-speech engine used is Google's "gTTS". To install: 'pip3 install gtts'
-If choosing to import a text file, make sure you use the full filename. For example. if
-you want to import a text file called 'input' you would enter input.txt. The file must 
-be located in the same directory as this python file.
+
+If choosing to import a text file, make sure you use the full path if the file is not in 
+the same directory as this program. For an example, if the text file was in my home
+directory I would enter /home/tim/textfilename.txt
 
 IMPORTANT INFO REGARDING TEXT FILES: Make sure desired text file has no empty spaces 
-at end of file, or you'll get an "AssertionError: No text to speak"
+at end of file, or you'll get an "AssertionError: No text to speak" (See to-do items).
 
 This program has only been tested with linux. I have no desire to make it work in 
 Windows or MacOS. If you do, be my guest.
@@ -23,8 +24,6 @@ Windows or MacOS. If you do, be my guest.
 To-do items:
     - Once files are made, do the nessassary conversion from inside program
     - GUI?
-    - Have user supply file location (path) instead of requiring the file to be in 
-      same directory as program.
     - Handle error when there are blank lines at bottom of text file (or just remove them).
 '''
 
@@ -48,7 +47,7 @@ while source != 'q':
     # line from file and generate MP3 files.
     if source == '1':
         list = []
-        txt_filename = str(input("\nPlease enter file name: "))
+        txt_filename = str(input("\nPlease enter the file's location: "))
         os.path.isfile(txt_filename)
         if os.path.isfile(txt_filename) == True:
             list = open(txt_filename).read().splitlines()
@@ -58,16 +57,16 @@ while source != 'q':
                 filename = list[x] + ".mp3"
                 tts.save(filename)
         else:
-            print("\nThe text file name you entered is not valid. Please try again. \n")
+            print("\nThe location you entered is not valid. Please try again. \n")
             sleep(2)
 
     # If user chooses to manually input text, ask how many items to process.
     elif source == '2':
         list = []
-        number = int(input("How many strings of text? "))
+        number = int(input("How many voice files would you like to create? "))
         for i in range(0, number):
             # Ask for manual input
-            word = str(input("Please enter a string: "))
+            word = str(input("Please enter a string of text: "))
             list.append(word)
         # create voice files from text entered 
         for x in range(len(list)):
