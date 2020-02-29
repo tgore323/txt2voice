a8bf2a1f0f3d4a9cfcebc886604255f7fc0a994b
Tim Gore <tgore323@gmail.com>
2020-02-29 11:14:27 -0800

    initial commit for converter.py
    This is the alpha code to convert the MP3s that gtts puts out into wav files

---
+++ converter.py
@@ -0,0 +1,16 @@
+import os
+import glob
+# files                                                                         
+lst = glob.glob("*.mp3")
+print(lst)
+ 
+for file in lst:
+# convert wav to mp3
+	os.system(f"""ffmpeg -i {file} -acodec pcm_u8 -ar 22050 {file[:-4]}.wav""") 
+# delete mp3 files after wav created
+currentdir = os.getcwd() + '/*.mp3'
+allmp3 = "rm *.mp3"
+os.system(allmp3)
+
+
+
