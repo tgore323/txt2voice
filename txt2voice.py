234d640dbd3a3fde4f831a737a6edcb09f7680f0
Tim Gore <tgore323@gmail.com>
2021-01-02 16:07:41 -0800

    fixed problem dealing with mixed case letters

--- txt2voice.py
+++ txt2voice.py
@@ -1,37 +1,7 @@
-'''
-txt2voice by Tim Gore, KE6QBV@galvonix.com
-version 0.5.4    3/1/2018
-
-txt2voice is a small python program to create channel announcement
-files for use with Motorola two-way radios. This program creates MP3 files
-which will need conversion to WAV files with proper formatting. (See
-to-do items).
-
-INSTRUCTIONS:
-
-The text-to-speech engine used is Google's "gTTS". To install: 'pip3 install gtts'
-
-If choosing to import a text file, make sure you use the full path if the file is not in
-the same directory as this program. For an example, if the text file was in my home
-directory I would enter /home/tim/textfilename.txt
-
-IMPORTANT INFO REGARDING TEXT FILES: Make sure desired text file has no empty spaces
-at end of file, or you'll get an "AssertionError: No text to speak" (See to-do items).
-
-This program has only been tested with linux. I have no desire to make it work in
-Windows or MacOS. If you do, be my guest.
-
-To-do items:
-    - Once files are made, do the nessassary conversion from inside program
-    - GUI?
-    - Handle error when there are blank lines at bottom of text file (or just remove them).
-'''
-
 from gtts import gTTS # import Google's TTS
 import os # for file system access
 from time import sleep # for delaying program output
 import glob
-
 
 print("\nHow would you like to supply the source text? \n")
 source = ''
@@ -61,6 +31,7 @@ while source != 'q':
                 tts = gTTS(list[x], lang = "en")
                 filename = list[x] + ".mp3"
                 filename = filename.replace(" ", "_")
+                filename = filename.lower()
                 tts.save(filename)
                 print("\nCreating file: " + filename)
                 lst = glob.glob("*.mp3")
@@ -83,6 +54,7 @@ while source != 'q':
         for i in range(0, number):
             # Ask for manual input
             word = str(input("Please enter a string of text: "))
+            word = word.lower()
             list.append(word)
         # create voice files from text entered
         for x in range(len(list)):
